f9998780cfbc4add6a88f8637cab382da06737b5
Zhengyi Yang <zhengyi.yang@outlook.com>
2017-05-02 22:33:42 +0100

    add flask-limiter and redis support

--- spy.py
+++ spy.py
@@ -5,24 +5,33 @@ Created on Mon Jan 23 15:47:53 2017
 @author: Zhengyi
 """
 from __future__ import unicode_literals
+import os
 import uuid
 import string
 from random import sample, randint
 import argparse
 
 from pydblite.sqlite import Database, Table
-from flask import Flask, session, redirect, url_for, render_template
+import redis
+from flask import Flask, session, redirect, url_for, render_template, request
 from flask_session import Session
 from flask_bootstrap import Bootstrap
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 from corpus import Corpus
+
+ENABLE_REDIS = True
+PROXY = True
 
 ROOM_ID_LEN = 5
 ID_RANGE = string.digits
 
-MAX_NUM_OF_USERS = 500
-MAX_NUM_OF_ROOMS = 200
 MAX_ROOM_SIZE = 10
+
+MAX_NUM_OF_RECORDS = {'users': 500, 'rooms': 500}
+REQUEST_LIMIT = {'create': '10/minute', 'room': '2/second'}
 
 words_500 = Corpus('corpora/words-500.txt', wsgi=False)
 
@@ -31,13 +40,13 @@ db = Database('WhoIsTheSpy.sqlite')
 users = Table('users', db)
 rooms = Table('rooms', db)
 
-if not 'users' in db:
+if 'users' not in db:
 
     users.create(('uuid', 'TEXT'), ('room', 'TEXT'),
                  ('num', 'INTEGER'))
     users.create_index('uuid')
 
-if not 'rooms' in db:
+if 'rooms' not in db:
 
     rooms.create(('room', 'TEXT'), ('civ_word', 'TEXT'),
                  ('spy_word', 'TEXT'), ('spy_num', 'INTEGER'),
@@ -46,19 +55,50 @@ if not 'rooms' in db:
     rooms.create_index('room')
 
 
+REDIS_URL = os.getenv('REDIS_URL', None)
+
 app = Flask(__name__)
-SESSION_TYPE = 'filesystem'
+
+RATELIMIT_HEADERS_ENABLED = True
+
+if REDIS_URL is None or ENABLE_REDIS is False:
+    SESSION_TYPE = 'filesystem'
+    RATELIMIT_STORAGE_URL = 'memory://'
+    redis_status = False
+else:
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = redis.from_url(REDIS_URL)
+    RATELIMIT_STORAGE_URL = REDIS_URL
+    redis_status = True
+
 app.config.from_object(__name__)
+
 Session(app)
 Bootstrap(app)
 
 
+def get_ip():
+    if request.access_route:
+        return request.access_route[-1]
+    else:
+        return request.remote_addr or '127.0.0.1'
+
+
+key_func = get_ip if PROXY else get_remote_address
+
+limiter = Limiter(
+    app,
+    key_func=key_func
+)
+
+
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
 
 @app.route('/room/<room_id>')
+@limiter.limit(REQUEST_LIMIT['room'])
 def enter(room_id):
     room_record = rooms(room=room_id)
     if not room_record:
@@ -86,7 +126,7 @@ def enter(room_id):
         else:
             users.insert(uid, room_id, user_num)
             users.commit()
-            db_clean(users, MAX_NUM_OF_USERS)
+            db_clean(users)
 
         user_record = users(uuid=uid)
 
@@ -114,6 +154,7 @@ def enter_():
 
 
 @app.route('/create/<int:total>')
+@limiter.limit(REQUEST_LIMIT['create'])
 def create(total):
     if total < 3:
         return error('需要至少3人')
@@ -128,7 +169,7 @@ def create(total):
     rooms.insert(room_id, civ_word, spy_word, randint(1, total),
                  total, 0, randint(1, total))
     rooms.commit()
-    db_clean(rooms, MAX_NUM_OF_ROOMS)
+    db_clean(rooms)
 
     url = url_for('enter', room_id=room_id)
     return redirect(url)
@@ -180,11 +221,28 @@ def rules():
     return render_template('rules.html')
 
 
-def error(msg):
-    return render_template('error.html', msg=msg)
+@app.route('/db/')
+def get_db_status():
+    return 'users:{}<br>rooms:{}'.format(len(users), len(rooms))
 
 
-def db_clean(table, max_records, delete_ratio=0.):
+@app.route('/redis/')
+def get_redis_status():
+    return str(redis_status)
+
+
+@app.route('/ip/')
+def what_is_my_ip():
+    return str(key_func())
+
+
+def error(msg, status_code=400):
+    return render_template('error.html', msg=msg), status_code
+
+
+def db_clean(table, max_records=None, delete_ratio=0.):
+    if max_records is None:
+        max_records = MAX_NUM_OF_RECORDS[table.name]
     num_of_records = len(table)
     if num_of_records > max_records:
         num_of_deletes = max(int(max_records * delete_ratio),
@@ -198,7 +256,12 @@ def db_clean(table, max_records, delete_ratio=0.):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('error.html', msg='找不到该页面（Error 404）'), 404
+    return error('找不到该页面 (Error 404)', 404)
+
+
+@app.errorhandler(429)
+def too_many_requests(e):
+    return error('访问过于频繁（Error 429)', 429)
 
 
 if __name__ == "__main__":
